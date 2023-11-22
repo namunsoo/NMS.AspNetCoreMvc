aeef079c33463a91aabe509de137975cbd31bc70
문수 나 <namunsoo@neoplus.co.kr>
2023-11-22 10:13:26 +0900

    데이터베이스

---
+++ BasicDB/dbo/Tables/BoardCategory.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[BoardCategory] (
+    [CategoryNo]   INT           IDENTITY (1, 1) NOT NULL,
+    [CategoryName] NVARCHAR (50) NOT NULL
+);
+
---
+++ BasicDB/dbo/Tables/BoardReply.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[BoardReply] (
+    [No]             INT              IDENTITY (1, 1) NOT NULL,
+    [BoardNo]        INT              NULL,
+    [ReplyNo]        INT              NULL,
+    [Text]           NVARCHAR (150)   NOT NULL,
+    [CreateUserCode] UNIQUEIDENTIFIER NOT NULL,
+    [CreateUserName] NVARCHAR (50)    NOT NULL,
+    [CreateDate]     DATETIME2 (7)    NOT NULL,
+    [UpdateUserCode] UNIQUEIDENTIFIER NULL,
+    [UpdateUserName] NVARCHAR (50)    NULL,
+    [UpdateDate]     DATETIME2 (7)    NULL
+);
+
