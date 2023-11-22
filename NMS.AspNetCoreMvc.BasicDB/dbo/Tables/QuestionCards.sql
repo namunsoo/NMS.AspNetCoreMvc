39acb7fed6ea8ed2a4c00d5876334631a2abc3d5
문수 나 <namunsoo@neoplus.co.kr>
2023-11-22 10:15:33 +0900

    데이터베이스

---
+++ NMS.AspNetCoreMvc.BasicDB/dbo/Tables/QuestionCards.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[QuestionCards] (
+    [No]         INT              IDENTITY (1, 1) NOT NULL,
+    [UserCode]   UNIQUEIDENTIFIER NOT NULL,
+    [Question]   NVARCHAR (MAX)   NOT NULL,
+    [Answer]     NVARCHAR (MAX)   NOT NULL,
+    [Importance] INT              NOT NULL,
+    [Memorize]   BIT              NOT NULL
+);
+
---
+++ NMS.AspNetCoreMvc.BasicDB/dbo/Tables/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserCode] UNIQUEIDENTIFIER NOT NULL,
+    [Name]     NVARCHAR (50)    NOT NULL,
+    [ID]       NVARCHAR (50)    NOT NULL,
+    [Password] VARCHAR (20)     NOT NULL
+);
+
